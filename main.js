10b5a1a7bfac30c14304e63f591bf7b37687d1e1
JavierIgnacio12 <j.lillohuircan@uandresbello.edu>
2023-11-18 13:41:37 -0300

    a

--- main.js
+++ main.js
@@ -1,39 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Reemplaza 'TU_API_KEY' con tu clave de API de Steam
     const apiKey = 'FA51A46803CA9086726A81362DBDF323';
+    
+    // Reemplaza 'STEAMID64_DEL_USUARIO' con el SteamID64 del usuario
+    const steamID = 'STEAMID64_DEL_USUARIO';
 
     // Reemplaza '34.176.188.153' con la dirección IP o el nombre de dominio correcto
-    const steamAPIURL = `https://34.176.188.153:3000/steamapi`;
-
-    // Obtener el SteamID64 del usuario después de que haya ingresado al servidor de Garry's Mod
-    const steamID64 = 'STEAMID64_DEL_USUARIO';
+    const steamAPIURL = `https://34.176.188.153:3000/steamapi?steamid=${steamID}`;
 
     // Realizar la solicitud a la API de Steam
-    fetch(`${steamAPIURL}?steamid=${steamID64}`, { method: 'GET', mode: 'cors' })
+    fetch(steamAPIURL)
         .then(response => response.json())
         .then(data => {
             // Actualizar la información en la página
             const user = data.response.players[0];
-            document.getElementById('steamAvatar').src = user ? user.avatarfull : 'https://i.pinimg.com/564x/66/c0/be/66c0bede32a10a2f017d789b259af478.jpg';
-            document.getElementById('steamID').textContent += user ? user.steamid : 'STEAMID_NO_DISPONIBLE';
+            const steamAvatar = user ? user.avatarfull : 'https://i.pinimg.com/564x/66/c0/be/66c0bede32a10a2f017d789b259af478.jpg';
+            const steamIDDisplay = user ? user.steamid : 'STEAMID_NO_DISPONIBLE';
+
+            // Mostrar información en la página
+            document.getElementById('steamAvatar').src = steamAvatar;
+            document.getElementById('steamID').textContent += steamIDDisplay;
         })
         .catch(error => {
             console.error('Error al obtener información de Steam:', error);
-            // Manejar el error aquí, por ejemplo, mostrar una imagen predeterminada o un mensaje de error
+            // Puedes manejar el error aquí, por ejemplo, mostrando una imagen predeterminada o un mensaje de error
             document.getElementById('steamAvatar').src = 'https://i.pinimg.com/564x/66/c0/be/66c0bede32a10a2f017d789b259af478.jpg';
             document.getElementById('steamID').textContent += 'STEAMID_NO_DISPONIBLE';
         });
-
-    // Configurar el sistema de red para recibir información del servidor
-    net.Receive('SendSteamInfo', function (len) {
-        const steamAvatar = net.ReadString();
-        const steamID = net.ReadString();
-
-        console.log('Steam Avatar:', steamAvatar);
-        console.log('Steam ID:', steamID);
-
-        // Actualizar la información en la página
-        document.getElementById('steamAvatar').src = steamAvatar;
-        document.getElementById('steamID').textContent += steamID;
-    });
 });
