b4ebeef94e7c6c6ed630cecc381e1ce7a2bfff38
JavierIgnacio12 <j.lillohuircan@uandresbello.edu>
2023-11-20 00:33:48 -0300

    a

---
+++ custom-script.js
@@ -0,0 +1,39 @@
+// custom-script.js
+
+// Steam API Key (reemplaza con tu clave de API)
+const apiKey = 'FA51A46803CA9086726A81362DBDF323';
+
+// Función para obtener la información del usuario de Steam y actualizar la lista
+function updateSteamUsersList() {
+    $('.name-list li').each(function() {
+        const steamId = $(this).data('steam-id');
+        const apiUrl = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamId}`;
+
+        // Realiza la solicitud
+        $.getJSON(apiUrl, data => {
+            const player = data.response.players[0];
+
+            // Actualiza el elemento de la lista con el avatar y el nombre de usuario
+            const listItem = `
+                <img src="${player.avatarfull}" alt="${player.personaname} Avatar">
+                <p>${player.personaname}</p>
+            `;
+            $(this).html(listItem);
+        });
+    });
+}
+
+// Llama a la función para actualizar la lista al cargar la página
+$(document).ready(() => {
+    updateSteamUsersList();
+});
+
+// Tu código para inicializar el visualizador de audio
+AUDIO.VISUALIZER.getInstance({
+    // Configuración del visualizador (ajusta según sea necesario)
+    autoplay: true,
+    loop: true,
+    audio: 'myAudio',
+    canvas: 'myCanvas',
+    // ... otras opciones
+});
