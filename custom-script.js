479363dcb44452080d8d78e63cc8d2f447d5b67b
JavierIgnacio12 <j.lillohuircan@uandresbello.edu>
2023-11-20 19:06:38 -0300

    a

--- custom-script.js
+++ custom-script.js
@@ -1,28 +1,36 @@
-// custom-script.js
-
 // Steam API Key (reemplaza con tu clave de API)
 const apiKey = 'FA51A46803CA9086726A81362DBDF323';
 
+// Arreglo con las ID de los elementos audio
+const audioElements = ['myAudio', 'myAudio1', 'myAudio2'];
+const currentAudioIndex = Math.floor(Math.random() * audioElements.length);
+
 // Función para obtener la información del usuario de Steam y actualizar la lista
 function updateSteamUsersList() {
-    $('.name-list li').each(function() {
+    $('.name-list li').each(function () {
         const steamId = $(this).data('steam-id');
-        const apiUrl = `https://34.176.188.153:3000/api/steam-user/${steamId}`;
+        const apiUrl = `http://34.176.188.153:3000/api/steam-user/${steamId}`;
 
         // Realiza la solicitud
-        $.getJSON(apiUrl, data => {
+        $.getJSON(apiUrl, (data) => {
             const player = data.response.players[0];
 
             // Actualiza el elemento de la lista con el avatar y el nombre de usuario
             const listItem = `
-            <div class="user-info">
-                <img src="${player.avatarfull}" alt="${player.personaname} Avatar">
-                <p>${player.personaname}</p>
-            </div>
+                <div class="user-info">
+                    <img src="${player.avatarfull}" alt="${player.personaname} Avatar">
+                    <p>${player.personaname}</p>
+                </div>
             `;
             $(this).html(listItem);
         });
     });
+}
+
+// Función para cargar una nueva canción al hacer clic en el canvas
+function loadRandomSong() {
+    const newIndex = (currentAudioIndex + 1) % audioElements.length; // Cambiar al siguiente audio
+    AUDIO.VISUALIZER.getInstance({ audio: audioElements[newIndex] }); // Cargar nueva canción
 }
 
 // Llama a la función para actualizar la lista al cargar la página
@@ -30,12 +38,15 @@ $(document).ready(() => {
     updateSteamUsersList();
 });
 
-// Tu código para inicializar el visualizador de audio
+// Llama a la función para inicializar el visualizador de audio
 AUDIO.VISUALIZER.getInstance({
     // Configuración del visualizador (ajusta según sea necesario)
     autoplay: true,
     loop: true,
-    audio: 'myAudio',
+    audio: audioElements[currentAudioIndex], // Selecciona el audio actual
     canvas: 'myCanvas',
     // ... otras opciones
 });
+
+// Evento de clic en el canvas para cambiar de canción
+$('#myCanvas').on('click', loadRandomSong);
